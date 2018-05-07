bff7480977a42b2b563c4f36c0d7a4a1f69fbc62
Moey Design <5884seiya@gmail.com>
2018-05-07 16:18:10 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<!-- =================================================
+ Template
+================================================== -->
+<template>
+  <div id="app">
+    app
+  </div>
+</template>
+
+<!-- =================================================
+ Script
+================================================== -->
+<script>
+export default {
+  name: 'App'
+}
+</script>
+。
+<!-- =================================================
+ Style
+================================================== -->
+<style lang='stylus'>
+@import 'reset-css/reset.css'
+@import '/stylus/base/*'
+@import '/stylus/fonts'
+@import '/stylus/global-styles'
+</style>
